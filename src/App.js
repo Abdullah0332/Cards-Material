f3b3c07e71d10ba1b3252b6a7d93b1c4af181b17
Abdullah Khan <abdullah.khan10032@gmail.com>
2020-10-07 16:37:39 +0500

    Materail UI Practice

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from './components/Card'
+
+export default function App() {
+  return (
+    <div>
+      <h1>PORTFOLIO</h1>
+      <p>Lorem ipsum dolor sit amet consectetur.</p>
+      <br />
+      <Card />
+    </div>
+  )
+}
